9fbd5b153b59b69c0ff5584b51faddbf43d6d027
agosileoluwafemi <69110803+agosileoluwafemi@users.noreply.github.com>
2026-02-08 15:06:03 +0100

    Add item search script
    This script checks if the number 13 is present in a predefined list.

---
+++ Part 2 script/Found.py
@@ -0,0 +1,7 @@
+# This program locates an item in a list. Its return true/false based on parameters.
+
+found = False
+for i in [32, 6, 12, 633, 54, 4] :
+    if i == 13 :
+        found = True
+print(found)
