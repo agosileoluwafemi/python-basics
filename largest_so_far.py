dc462067bedae0e428d10e82eb1bb496d1724009
agosileoluwafemi <69110803+agosileoluwafemi@users.noreply.github.com>
2026-02-08 13:51:45 +0100

    Add largest_so_far python script to find largest number
    Implement logic to find and print the largest number from a list.

---
+++ largest_so_far.py
@@ -0,0 +1,8 @@
+largest_so_far = None
+for i in [32, 6, 12, 633, 54, 4] :
+    if largest_so_far is None :
+        largest_so_far = i
+    elif i > largest_so_far :
+        largest_so_far = i
+    print(i, largest_so_far)
+print('The largest so far is', largest_so_far)
